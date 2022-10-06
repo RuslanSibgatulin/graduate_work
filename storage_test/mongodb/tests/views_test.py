986135d8653f362084272259ddb0e78a2aae8680
Ruslan Sibgatulin <doolf@yandex.ru>
2022-10-06 16:33:28 +0300

    Tests fix

--- storage_test/mongodb/tests/views_test.py
+++ storage_test/mongodb/tests/views_test.py
@@ -9,12 +9,12 @@ pytestmark = pytest.mark.asyncio
 # @pytest.mark.skip(reason="Insert disabled")
 @pytest.mark.parametrize(
     "users_count",
-    [100000, 100000, 100000]
+    [1000, 10000, 100000, 100000, 100000, 100000, 89000]
 )
 async def test_insert_users_with_views(
     mongo_db, users_list, movies_list, users_count
 ):
-    print(f"Fill views collection. {users_count} docs. ", end="\n")
+    pprint(f"Fill views collection. {users_count} docs. ")
     max_viewed = 50
 
     users_with_views = random.choices(users_list, k=users_count)
@@ -30,12 +30,9 @@ async def test_insert_users_with_views(
             } for user in users_with_views
         ]
     )
-    n = len(result.inserted_ids)
-
-    print(f"\r\tInserted docs | {n} ", end="\r")
 
     # Проверка результата
-    # assert n == users_count
+    assert len(result.inserted_ids) == users_count
 
 
 @pytest.mark.parametrize(
