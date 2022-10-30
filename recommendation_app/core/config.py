d923b2b90bd65c733e3e8647a5217bb151947ae8
riyce <riyce@mail.ru>
2022-10-30 23:21:56 +0300

    fix

--- recommendation_app/core/config.py
+++ recommendation_app/core/config.py
@@ -7,7 +7,7 @@ class Config(BaseSettings):
     mongo_host: str = "127.0.0.1"
     mongo_port: int = 27017
     mongo_db: str = "like"
-    mongo_user_collection: str = "user"
+    mongo_user_collection: str = "users"
     user_genres_check_url: str = "http://127.0.0.1/api/v1/auth/genres"
     url_movies_by_genre: str = "http://127.0.0.1/api/v1/film"
     url_movies_name: str = "http://127.0.0.1/api/v1/film/names"
